8e5e566dd1c02669c748f3a6f0d6279d597d5914
tobiasBora <tobias.bora@gmail.com>
2019-03-06 21:29:06 +0100

    perlPackage.TermShell: init at 1.4.0

--- pkgs/top-level/perl-packages.nix
+++ pkgs/top-level/perl-packages.nix
@@ -14646,6 +14646,21 @@ let
     };
   };
 
+  TermShell = buildPerlModule rec {
+    name = "Term-Shell-0.10";
+    src = fetchurl {
+      url = "mirror://cpan/authors/id/S/SH/SHLOMIF/${name}.tar.gz";
+      sha256 = "7d1f824c2db22769b60000b5b9ca2ad469c154939f9ec1cd3f0e06e9c967dda3";
+    };
+    buildInputs = [ ModuleBuild ];
+    propagatedBuildInputs = [ TermReadKey TextAutoformat ];
+    meta = {
+      homepage = http://metacpan.org/release/Term-Shell;
+      description = "A simple command-line shell framework";
+      license = with stdenv.lib.licenses; [ artistic1 gpl1Plus ];
+    };
+  };
+
   TermShellUI = buildPerlPackage rec {
     name = "Term-ShellUI-0.92";
     src = fetchurl {
